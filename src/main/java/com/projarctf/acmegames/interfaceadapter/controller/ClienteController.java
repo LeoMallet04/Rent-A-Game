78c9d2669954e2d178d1e8f26186b046bb553f73
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-06-05 19:44:15 -0300

    Adição da camada de InterfaceAdapter

---
+++ src/main/java/com/projarctf/acmegames/interfaceadapter/controller/ClienteController.java
@@ -0,0 +1,4 @@
+package com.projarctf.acmegames.interfaceadapter.controller;
+
+public class ClienteController {
+}
---
+++ src/main/java/com/projarctf/acmegames/interfaceadapter/dto/ClienteDTO.java
@@ -0,0 +1,4 @@
+package com.projarctf.acmegames.interfaceadapter.dto;
+
+public class ClienteDTO {
+}
