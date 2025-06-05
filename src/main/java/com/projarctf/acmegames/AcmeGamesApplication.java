5ae5ec5a6b974e42a5dcdec8b2fd690ed00727ed
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-06-05 17:17:01 -0300

    Inicializando Springboot

---
+++ src/main/java/com/projarctf/acmegames/AcmeGamesApplication.java
@@ -0,0 +1,13 @@
+package com.projarctf.acmegames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmeGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcmeGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projarctf/acmegames/AcmeGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projarctf.acmegames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmeGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
