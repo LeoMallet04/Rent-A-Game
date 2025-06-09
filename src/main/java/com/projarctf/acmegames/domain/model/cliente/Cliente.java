5cebe2e61848f4f696297e17ad39cb8dc5022683
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-06-09 13:39:39 -0300

    Criação do objeto de dominio Cliente

---
+++ src/main/java/com/projarctf/acmegames/domain/model/cliente/Cliente.java
@@ -0,0 +1,10 @@
+package com.projarctf.acmegames.domain.model.cliente;
+
+import lombok.Data;
+
+@Data
+public class Cliente {
+    private int numero;
+    private String nome;
+    private String endereco;
+}
