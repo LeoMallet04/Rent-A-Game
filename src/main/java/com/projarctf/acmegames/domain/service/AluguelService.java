ed7592d91ba6a95a605cd6e3e96cb61025adbba2
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-06-15 16:02:34 -0300

    Foi adicionado a lógica de calcular valor do aluguel para um método na propria entidade e agora é chamado ele na Service

--- src/main/java/com/projarctf/acmegames/domain/service/AluguelService.java
+++ src/main/java/com/projarctf/acmegames/domain/service/AluguelService.java
@@ -1,27 +1,19 @@
 package com.projarctf.acmegames.domain.service;
 
-import com.projarctf.acmegames.domain.model.Jogo;
-import com.projarctf.acmegames.domain.model.JogoEletronico;
+import com.projarctf.acmegames.domain.model.aluguel.Aluguel;
+import com.projarctf.acmegames.domain.model.cliente.Individual;
+import com.projarctf.acmegames.domain.model.jogo.Jogo;
+import com.projarctf.acmegames.domain.model.jogo.JogoEletronico;
 import com.projarctf.acmegames.domain.model.cliente.Cliente;
 import com.projarctf.acmegames.domain.model.cliente.Empresarial;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AluguelService {
-    public double calcularValorAluguel(Cliente cliente, Jogo jogo, int duracaoDias) {
-        if (cliente instanceof Empresarial) {
-            if (duracaoDias < 7) { // 0-6
-                return (jogo.calculaValorAluguel() * 0.9 * duracaoDias);
-            } else if (duracaoDias <= 14) { // 7-14
-                return (jogo.calculaValorAluguel() * 0.8 * duracaoDias);
-            } else { // 15+
-                return (jogo.calculaValorAluguel() * 0.75 * duracaoDias);
-            }
-        } else {
-            if (jogo instanceof JogoEletronico) {
-                return jogo.calculaValorAluguel() * duracaoDias;
-            } else { // mesa
-                return jogo.calculaValorAluguel() * duracaoDias * 0.85;
-            }
-        }
+    private Aluguel aluguel;
+
+    public double calculaValorAluguel(){
+        return aluguel.calcularValorAluguel();
     }
 }
  
