932dd5ebfff14bb6e690832b9eaba1d4a0c78769
Felipe Valiati <felipe.cvaliati@gmail.com>
2025-06-15 18:46:07 -0300

    feat eletronicoentity

---
+++ src/main/java/com/projarctf/acmegames/infraestructure/persistence/entity/EletronicoEntity.java
@@ -0,0 +1,15 @@
+package com.projarctf.acmegames.infraestructure.persistence.entity;
+
+import com.projarctf.acmegames.domain.enums.TipoEletronico;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class EletronicoEntity extends JogoEntity {
+    private TipoEletronico tipo;
+    private String plataforma;
+}
