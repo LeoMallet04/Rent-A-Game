a0f8d3515b2c7502af018cae54ff43964e508f76
Felipe Valiati <felipe.cvaliati@gmail.com>
2025-06-15 18:30:52 -0300

    refact aluguel is now a parameter in aluguel service

--- src/main/java/com/projarctf/acmegames/domain/service/AluguelService.java
+++ src/main/java/com/projarctf/acmegames/domain/service/AluguelService.java
@@ -1,18 +1,11 @@
 package com.projarctf.acmegames.domain.service;
 
 import com.projarctf.acmegames.domain.model.aluguel.Aluguel;
-import com.projarctf.acmegames.domain.model.cliente.Individual;
-import com.projarctf.acmegames.domain.model.jogo.Jogo;
-import com.projarctf.acmegames.domain.model.jogo.JogoEletronico;
-import com.projarctf.acmegames.domain.model.cliente.Cliente;
-import com.projarctf.acmegames.domain.model.cliente.Empresarial;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AluguelService {
-    private Aluguel aluguel;
-
-    public double calculaValorAluguel(){
+    public double calculaValorAluguel(Aluguel aluguel){
         return aluguel.calcularValorAluguel();
     }
 }
