d4d627454a286396b4309d7ae624f21159a8b241
Felipe Valiati <felipe.cvaliati@gmail.com>
2025-06-15 18:50:58 -0300

    feat mesa entity

---
+++ src/main/java/com/projarctf/acmegames/infraestructure/persistence/entity/MesaEntity.java
@@ -0,0 +1,15 @@
+package com.projarctf.acmegames.infraestructure.persistence.entity;
+
+import com.projarctf.acmegames.domain.enums.TipoMesa;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class MesaEntity extends JogoEntity {
+    private TipoMesa tipo;
+    private int numeroPecas;
+}
