f8146c12d7f05c0374eca4cbcfd7cab50afb0dca
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-06-16 11:36:23 -0300

    Remoção da lista de alugueis no Cliente e no Jogo

--- src/main/java/com/projarctf/acmegames/domain/model/cliente/Cliente.java
+++ src/main/java/com/projarctf/acmegames/domain/model/cliente/Cliente.java
@@ -1,9 +1,6 @@
 package com.projarctf.acmegames.domain.model.cliente;
 
-import com.projarctf.acmegames.domain.model.aluguel.Aluguel;
 import lombok.*;
-
-import java.util.List;
 
 @Getter
 @Setter
@@ -13,5 +10,4 @@ public class Cliente {
     private int numero;
     private String nome;
     private String endereco;
-    private List<Aluguel> alugueis;
 }
--- src/main/java/com/projarctf/acmegames/domain/model/jogo/Jogo.java
+++ src/main/java/com/projarctf/acmegames/domain/model/jogo/Jogo.java
@@ -1,9 +1,6 @@
 package com.projarctf.acmegames.domain.model.jogo;
 
-import com.projarctf.acmegames.domain.model.aluguel.Aluguel;
 import lombok.*;
-
-import java.util.List;
 
 @Getter
 @Setter
@@ -13,7 +10,6 @@ public abstract class Jogo {
     private int codigo;
     private String nome;
     private double valorBase;
-    private List<Aluguel> alugueis;
 
     public abstract double calculaValorAluguel();
 }
