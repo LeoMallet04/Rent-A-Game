b3db28367f61885bd1d00b521f7a130d253cbd77
Felipe Valiati <felipe.cvaliati@gmail.com>
2025-06-17 15:22:26 -0300

    feat jogo controller

--- src/main/java/com/projarctf/acmegames/adapter/controller/JogoController.java
+++ src/main/java/com/projarctf/acmegames/adapter/controller/JogoController.java
@@ -1,10 +1,25 @@
 package com.projarctf.acmegames.adapter.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.projarctf.acmegames.application.usecases.ListaJogoUsecase;
+import com.projarctf.acmegames.domain.model.jogo.Jogo;
+
 @RestController
-@RequestMapping("/jogos")
+@RequestMapping("/acmegames/cadastro")
 public class JogoController {
-    
+
+    @Autowired
+    private ListaJogoUsecase listaJogoUsecase;
+
+    @GetMapping("/listajogos")
+    public ResponseEntity<List<Jogo>> listaJogos() {
+        return listaJogoUsecase.listarJogos();
+    }
 }
