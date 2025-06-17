8584203e4a0ea927de0cd7027fc762b26267fdac
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-06-16 22:43:19 -0300

    Criação da Controller de Aluguel

---
+++ src/main/java/com/projarctf/acmegames/adapter/controller/AluguelController.java
@@ -0,0 +1,11 @@
+package com.projarctf.acmegames.adapter.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/acmegames/cadastro")
+@RestController
+public class AluguelController {
+
+}
