c0892aa0032708a4d44f35f5cda1f95333a2bb98
Felipe Valiati <felipe.cvaliati@gmail.com>
2025-06-17 15:22:36 -0300

    feat jogo service

--- src/main/java/com/projarctf/acmegames/domain/service/JogoService.java
+++ src/main/java/com/projarctf/acmegames/domain/service/JogoService.java
@@ -2,11 +2,31 @@ package com.projarctf.acmegames.domain.service;
 
 import java.util.List;
 
-import com.projarctf.acmegames.infraestructure.persistence.entity.JogoEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.projarctf.acmegames.domain.model.jogo.Jogo;
+import com.projarctf.acmegames.infraestructure.persistence.entity.JogoEntity;
+import com.projarctf.acmegames.infraestructure.persistence.repository.JogoRepoJpaImpl;
+import com.projarctf.acmegames.infraestructure.persistence.repository.mapper.JogoMapper;
+
+@Service
 public class JogoService {
-    public List<JogoEntity> getJogos() {
+    @Autowired
+    JogoRepoJpaImpl jogoRepository;
+
+    @Autowired
+    JogoMapper jogoMapper;
+
+    public List<Jogo> listJogos() {
         
-        return null; // Retornar a lista de jogos
+        List<JogoEntity> jogoEntities = jogoRepository.getJogos();
+
+        List<Jogo> jogos = jogoEntities.stream()
+            .map(jogoEntity -> jogoMapper.toDomain(jogoEntity)
+                )
+            .toList();
+
+        return jogos;
     }
 }
