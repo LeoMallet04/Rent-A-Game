a75bdfd2a7af07058c392051b9ff430635f47752
Felipe Valiati <felipe.cvaliati@gmail.com>
2025-06-17 15:24:07 -0300

    fix wrong method being called

--- src/main/java/com/projarctf/acmegames/application/usecases/ListaJogoUsecase.java
+++ src/main/java/com/projarctf/acmegames/application/usecases/ListaJogoUsecase.java
@@ -2,14 +2,14 @@ package com.projarctf.acmegames.application.usecases;
 
 import java.util.List;
 
+import com.projarctf.acmegames.domain.model.jogo.Jogo;
 import com.projarctf.acmegames.domain.service.JogoService;
-import com.projarctf.acmegames.infraestructure.persistence.entity.JogoEntity;
 
 public class ListaJogoUsecase {
     public List<Jogo> listarJogos() {
         JogoService jogoService = new JogoService();
         
-        List<Jogo> jogos = jogoService.getJogos();
+        List<Jogo> jogos = jogoService.listJogos();
 
         return jogos;
     }
