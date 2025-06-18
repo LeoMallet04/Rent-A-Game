df890adb46c3248bd00984a9b1c8028fb9a2c5b0
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-06-17 23:10:17 -0300

    Implementação da AluguelJpaRepositoryImpl

--- src/main/java/com/projarctf/acmegames/infraestructure/persistence/repository/aluguel/AluguelJpaRepositoryImpl.java
+++ src/main/java/com/projarctf/acmegames/infraestructure/persistence/repository/aluguel/AluguelJpaRepositoryImpl.java
@@ -1,27 +1,28 @@
  package com.projarctf.acmegames.infraestructure.persistence.repository.aluguel;
 
- import com.projarctf.acmegames.domain.model.aluguel.Aluguel;
- import com.projarctf.acmegames.domain.repository.IAluguelRepository;
- import lombok.Getter;
- import lombok.Setter;
- import org.springframework.stereotype.Repository;
+import com.projarctf.acmegames.domain.model.aluguel.Aluguel;
+import com.projarctf.acmegames.domain.repository.IAluguelRepository;
+import com.projarctf.acmegames.infraestructure.persistence.repository.mapper.AluguelMapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
  import java.util.List;
-
- @Getter
- @Setter
+import java.util.stream.Collectors;
  @Repository
  public class AluguelJpaRepositoryImpl implements IAluguelRepository {
 
-    private IAluguelJpaItf repository;
+    @Autowired
+    private IAluguelJpaItfRep repository;
 
-    public AluguelJpaRepositoryImpl(IAluguelJpaItf repository){
+    public AluguelJpaRepositoryImpl(IAluguelJpaItfRep repository){
         this.repository = repository;
     }
+    
 
 
    @Override
     public List<Aluguel> getAlugueis(){
-        return repository.findAll();
+        return repository.findAll().stream().map(entity -> AluguelMapper.toModel(entity)).collect(Collectors.toList());
    }
  }
