58d7b7861199afa5ab6b6609601aab606c7acdac
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-06-17 23:12:17 -0300

    Remoção da EnableJpaRepositories

--- src/main/java/com/projarctf/acmegames/AcmeGamesApplication.java
+++ src/main/java/com/projarctf/acmegames/AcmeGamesApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories(basePackages={"src.main.java.com.projarctf.acmegames.infraestructure.repository"})
 @SpringBootApplication
 public class AcmeGamesApplication {
 
