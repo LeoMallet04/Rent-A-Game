f858413eb5522478221a60f25f71a72d4311c6a4
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-06-17 23:10:50 -0300

    Implementação do IAluguelJpaItfRep

---
+++ src/main/java/com/projarctf/acmegames/infraestructure/persistence/repository/aluguel/IAluguelJpaItfRep.java
@@ -0,0 +1,12 @@
+package com.projarctf.acmegames.infraestructure.persistence.repository.aluguel;
+
+import com.projarctf.acmegames.infraestructure.persistence.entity.AluguelEntity;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface IAluguelJpaItfRep extends CrudRepository<AluguelEntity, Integer> {
+    List<AluguelEntity> findAll();
+    
+}
