ff2826f786d2c7b9ede2b4398f997613171a47be
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-06-17 23:12:29 -0300

    Criação do AluguelController

--- src/main/java/com/projarctf/acmegames/adapter/controller/AluguelController.java
+++ src/main/java/com/projarctf/acmegames/adapter/controller/AluguelController.java
@@ -1,11 +1,24 @@
 package com.projarctf.acmegames.adapter.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.projarctf.acmegames.application.dto.AluguelDTO;
+
+import com.projarctf.acmegames.application.usecases.ListarAlugueisUseCase;
 
 @RequestMapping("/acmegames/cadastro")
 @RestController
 public class AluguelController {
 
+    @Autowired
+    private ListarAlugueisUseCase listarAlugueisUseCase;
+
+    @GetMapping("/acmegames/cadastro/listaalugueis")
+    public List<AluguelDTO> getAlugueis(){
+        return listarAlugueisUseCase.execute();
+    }
 }
