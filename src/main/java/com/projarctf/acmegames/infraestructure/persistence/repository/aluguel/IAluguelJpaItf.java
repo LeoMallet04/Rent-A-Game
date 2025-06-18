65c60456399c16d92823f6e314dcafa42c88e0d2
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-06-17 23:07:51 -0300

    Alguns ajustes

--- src/main/java/com/projarctf/acmegames/infraestructure/persistence/repository/aluguel/IAluguelJpaItf.java
+++
@@ -1,7 +0,0 @@
-package com.projarctf.acmegames.infraestructure.persistence.repository.aluguel;
-
-import com.projarctf.acmegames.infraestructure.persistence.entity.AluguelEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IAluguelJpaItf extends CrudRepository<AluguelEntity, Integer> {
-}
