34bb7cdc1302251912d2cc676248b69af0c9f79f
Felipe Valiati <felipe.cvaliati@gmail.com>
2025-06-19 21:26:16 -0300

    chore remove import

--- src/main/java/com/projarctf/acmegames/adapter/controller/JogoController.java
+++ src/main/java/com/projarctf/acmegames/adapter/controller/JogoController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projarctf.acmegames.application.usecases.ListaJogoUsecase;
-import com.projarctf.acmegames.domain.model.jogo.Jogo;
 
 @RestController
 @RequestMapping("/acmegames/cadastro")
