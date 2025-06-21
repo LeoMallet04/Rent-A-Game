0e5aca3afccb1c6e62ac50f7cfacac83f1040e7e
Felipe Valiati <felipe.cvaliati@gmail.com>
2025-06-21 15:30:05 -0300

    fix wrong dependency

--- src/main/java/com/projarctf/acmegames/domain/service/JogoService.java
+++ src/main/java/com/projarctf/acmegames/domain/service/JogoService.java
@@ -6,12 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.projarctf.acmegames.domain.model.jogo.Jogo;
-import com.projarctf.acmegames.infrastructure.persistence.repository.jogo.JogoRepoJpaImpl;
+import com.projarctf.acmegames.domain.repository.IJogoRepository;
 
 @Service
 public class JogoService {
     @Autowired
-    JogoRepoJpaImpl jogoRepository;
+    IJogoRepository jogoRepository;
 
     public List<Jogo> listJogos() {
         return jogoRepository.getJogos();
