ae27a791d77d7ffeb45be6d3f16ee4ab30faa906
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-06-22 16:37:54 -0300

    Criação da UseCase para validar aluguel

---
+++ src/main/java/com/projarctf/acmegames/application/usecases/aluguel/ValidaAluguelUseCase.java
@@ -0,0 +1,17 @@
+package com.projarctf.acmegames.application.usecases.aluguel;
+
+import com.projarctf.acmegames.domain.service.AluguelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ValidaAluguelUseCase {
+
+    @Autowired
+    private AluguelService aluguelService;
+
+    public boolean validaAluguel(int identificador){
+        return aluguelService.validaAluguel(identificador);
+    }
+
+}
