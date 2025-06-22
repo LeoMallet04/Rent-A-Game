26e7bc8081e26f02386ad12603824ecce5202e1f
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-06-22 16:32:47 -0300

    Criação de endpoints para validar e cadastrar alugueis

--- src/main/java/com/projarctf/acmegames/adapter/controller/AluguelController.java
+++ src/main/java/com/projarctf/acmegames/adapter/controller/AluguelController.java
@@ -2,23 +2,44 @@ package com.projarctf.acmegames.adapter.controller;
 
 import java.util.List;
 
+import com.projarctf.acmegames.application.dto.AluguelCreateDTO;
+import com.projarctf.acmegames.application.usecases.aluguel.CadastraAluguelUseCase;
+import com.projarctf.acmegames.application.usecases.aluguel.ValidaAluguelUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.projarctf.acmegames.application.dto.AluguelDTO;
+import com.projarctf.acmegames.application.usecases.aluguel.ListaAlugueisUseCase;
+import com.projarctf.acmegames.domain.model.aluguel.Aluguel;
 
-import com.projarctf.acmegames.application.usecases.ListarAlugueisUseCase;
-
-@RequestMapping("/acmegames/cadastro")
+@RequestMapping("/acmegames")
 @RestController
 public class AluguelController {
 
     @Autowired
-    private ListarAlugueisUseCase listarAlugueisUseCase;
+    private ListaAlugueisUseCase listarAlugueisUseCase;
 
-    @GetMapping("/listaalugueis")
+    @Autowired
+    private ValidaAluguelUseCase validaAluguelUseCase;
+
+    @Autowired
+    private CadastraAluguelUseCase cadastraAluguelUseCase;
+
+    @GetMapping("/cadastro/listaalugueis")
     public List<AluguelDTO> getAlugueis(){
-        return listarAlugueisUseCase.execute();
+        return listarAlugueisUseCase.listarAlugueis();
+    }
+
+    @PostMapping("/cadastro/cadaluguel")
+    public boolean cadastraAluguel(@RequestBody AluguelCreateDTO aluguelCreateDTO){
+        return cadastraAluguelUseCase.cadastraAluguel(aluguelCreateDTO);
+    }
+
+    @PostMapping("/validaaluguel")
+    public boolean validaAluguel(@RequestBody int identificador){
+        return validaAluguelUseCase.validaAluguel(identificador);
     }
 }
