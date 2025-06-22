f00ac05f1a4dcb7d23da91487305775467bddf56
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-06-22 16:37:41 -0300

    Criação da UseCase para cadastrar Aluguel

---
+++ src/main/java/com/projarctf/acmegames/application/usecases/aluguel/CadastraAluguelUseCase.java
@@ -0,0 +1,32 @@
+ package com.projarctf.acmegames.application.usecases.aluguel;
+
+ import com.projarctf.acmegames.application.assembler.AluguelAssembler;
+ import com.projarctf.acmegames.application.dto.AluguelCreateDTO;
+ import com.projarctf.acmegames.application.dto.AluguelDTO;
+ import com.projarctf.acmegames.domain.model.cliente.Cliente;
+ import com.projarctf.acmegames.domain.model.jogo.Jogo;
+ import com.projarctf.acmegames.domain.service.AluguelService;
+ import com.projarctf.acmegames.domain.service.ClienteService;
+ import com.projarctf.acmegames.domain.service.JogoService;
+ import org.springframework.beans.factory.annotation.Autowired;
+ import org.springframework.stereotype.Service;
+
+ @Service
+ public class CadastraAluguelUseCase {
+
+     @Autowired
+     private AluguelService aluguelService;
+
+     @Autowired
+     private ClienteService clienteService;
+
+     @Autowired
+     private JogoService jogoService;
+
+     public boolean cadastraAluguel(AluguelCreateDTO aluguelCreateDTO){
+         Cliente cliente = clienteService.getClientePorNumero(aluguelCreateDTO.getNumeroCliente());
+         Jogo jogo = jogoService.getJogoPorCodigo(aluguelCreateDTO.getCodigoJogo());
+
+         return aluguelService.cadastraAluguel(AluguelAssembler.CreateDTOtoDomain(aluguelCreateDTO, cliente,jogo));
+     }
+ }
