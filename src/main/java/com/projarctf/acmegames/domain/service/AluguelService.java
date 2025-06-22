6753443a3704c53ad70801350b334b74324d0975
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-06-22 16:35:26 -0300

    Novos serviços de cadastro e validação de alugueis no AluguelService

--- src/main/java/com/projarctf/acmegames/domain/service/AluguelService.java
+++ src/main/java/com/projarctf/acmegames/domain/service/AluguelService.java
@@ -1,24 +1,40 @@
-// package com.projarctf.acmegames.domain.service;
+ package com.projarctf.acmegames.domain.service;
 
-// import com.projarctf.acmegames.application.dto.AluguelDTO;
-// import com.projarctf.acmegames.domain.model.aluguel.Aluguel;
-// import com.projarctf.acmegames.infrastructure.persistence.repository.aluguel.AluguelJpaRepositoryImpl;
+ import com.projarctf.acmegames.application.dto.AluguelDTO;
+ import com.projarctf.acmegames.domain.model.aluguel.Aluguel;
+ import com.projarctf.acmegames.domain.repository.IAluguelRepository;
+ import com.projarctf.acmegames.domain.repository.IJogoRepository;
+ import com.projarctf.acmegames.infrastructure.persistence.repository.aluguel.AluguelJpaRepositoryImpl;
 
-// import org.springframework.stereotype.Service;
+ import org.springframework.beans.factory.annotation.Autowired;
+ import org.springframework.stereotype.Component;
+ import org.springframework.stereotype.Service;
 
-// import java.util.List;
+ import java.util.List;
 
-// @Service
-// public class AluguelService {
+ @Component
+ public class AluguelService {
 
-//     private AluguelJpaRepositoryImpl repository;
-    
-//     public List<AluguelDTO> getAlugueis(){
-        
+     @Autowired
+     private IAluguelRepository aluguelRepository;
+
+     public List<Aluguel> listAlugueis(){
+         return aluguelRepository.getAlugueis();
+     }
+
+     public boolean cadastraAluguel(Aluguel aluguel){
+         return aluguelRepository.cadastraAluguel(aluguel);
+     }
+
+     public boolean validaAluguel(int identificador){
+        if(aluguelRepository.findAluguelById(identificador) != null){
+            return true;
+        }
+        return false;
+     }
+
+//     public double getValorAluguelPorJogo(int codigo){
+//         return aluguelRepository.getValorAluguelPorJogo(codigo);
 //     }
-
-//     public double calculaValorAluguel(Aluguel aluguel){
-//         return aluguel.calcularValorAluguel();
-//     }
-// }
+ }
  
