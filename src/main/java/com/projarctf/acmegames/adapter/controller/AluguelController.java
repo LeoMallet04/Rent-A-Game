cc89d4597fddba024b3eaccac9afc4a74101c7d2
LeoMallet04 <leonardoms.2010@hotmail.com>
2025-06-23 19:24:43 -0300

    Remoção de controllers desnecessários

--- src/main/java/com/projarctf/acmegames/adapter/controller/AluguelController.java
+++ src/main/java/com/projarctf/acmegames/adapter/controller/AluguelController.java
@@ -2,17 +2,14 @@ package com.projarctf.acmegames.adapter.controller;
 
 import java.util.List;
 
+import com.projarctf.acmegames.application.dto.AluguelClienteDTO;
 import com.projarctf.acmegames.application.dto.AluguelCreateDTO;
-import com.projarctf.acmegames.application.usecases.aluguel.CadastraAluguelUseCase;
-import com.projarctf.acmegames.application.usecases.aluguel.ValidaAluguelUseCase;
+import com.projarctf.acmegames.application.usecases.aluguel.*;
+import com.projarctf.acmegames.application.usecases.jogo.ListaAlugueisDoClienteUseCase;
+import com.projarctf.acmegames.application.usecases.jogo.ListaAlugueisDoJogoUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.projarctf.acmegames.application.dto.AluguelDTO;
-import com.projarctf.acmegames.application.usecases.aluguel.ListaAlugueisUseCase;
 
 @RequestMapping("/acmegames")
 @RestController
@@ -27,10 +24,24 @@ public class AluguelController {
     @Autowired
     private CadastraAluguelUseCase cadastraAluguelUseCase;
 
+    @Autowired
+    private ListaAlugueisDoClienteUseCase listaAlugueisDoClienteUseCase;
+
+    @Autowired
+    private ListaAlugueisDoJogoUseCase listaAlugueisDoJogoUseCase;
+
+    @Autowired
+    private ValorAluguelJogoUseCase valorAluguelJogoUseCase;
+
+    @Autowired
+    private ValorAluguelFinalUseCase valorAluguelFinalUseCase;
+
+
     @GetMapping("/cadastro/listaalugueis")
     public List<AluguelDTO> getAlugueis(){
         return listarAlugueisUseCase.listarAlugueis();
     }
+
 
     @PostMapping("/cadastro/cadaluguel")
     public boolean cadastraAluguel(@RequestBody AluguelCreateDTO aluguelCreateDTO){
@@ -41,4 +52,25 @@ public class AluguelController {
     public boolean validaAluguel(@RequestBody int identificador){
         return validaAluguelUseCase.validaAluguel(identificador);
     }
+
+    @GetMapping("/cliente/aluguel/:{numero}")
+    public List<AluguelClienteDTO> listaAlugueisDoCliente(@PathVariable(value = "numero") final int numero) {
+        return listaAlugueisDoClienteUseCase.listarAlugueisDoCliente(numero); // Exemplo de código do cliente
+    }
+
+    @GetMapping("/jogo/aluguel/:{codigo}")
+    public List<AluguelDTO> listaAlugueisDoJogo(@PathVariable(value = "codigo") final int codigo) {
+        return listaAlugueisDoJogoUseCase.listarAlugueisDoJogo(codigo);
+    }
+
+    @GetMapping("/aluguel/valorjogo/:{codigo}")
+    public double getValorAluguelPorJogo(@PathVariable(value = "codigo") int codigo){
+        return valorAluguelJogoUseCase.getValorAluguelPorJogo(codigo);
+    }
+
+    @GetMapping("/aluguel/valorfinal/:{identificador}")
+    public double gerValorAluguelFinal(@PathVariable(value = "identificador") int identificador){
+        return valorAluguelFinalUseCase.getValorAluguelFinal(identificador);
+    }
+
 }
